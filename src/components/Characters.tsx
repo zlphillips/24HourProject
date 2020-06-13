27d9073d80ac38d50720d1200dc1856455b8a090
corynnem <corynnem@users.noreply.github.com>
2020-06-13 11:36:51 -0400

    first push character component

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,1 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+
+interface Character {
+    name: string,
+    gender: string,
+    born: Date,
+    died: Date,
+    aliases: string
+}
+
+interface CharactersProps{
+
+}
+
+interface CharactersState{
+    characters: Array<Character>
+}
+
+class Characters extends Component <CharactersProps, CharactersState>{
+   constructor(props: CharactersProps){
+       super(props);
+       this.state = {
+            characters: []
+       } 
+
+   }
+
+     fetchResults(){
+        fetch("https://www.anapioficeandfire.com/api/characters")
+        .then((results) => results.json())
+        .then((results) => this.setState({
+            characters: results
+        }))
+    }
+    
+
+    componentDidMount() {
+        setTimeout(() => {
+          this.fetchResults();
+        }, 1000);
+      }
+    
+    render() {
+        return(
+            <div>
+                <h1>Characters</h1>
+            </div>
+        )
+    }
+}
+
+export default Characters
