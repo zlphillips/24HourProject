6eaffb8c04566058482953cb74bd7512443f9589
zlphillips <60951969+zlphillips@users.noreply.github.com>
2020-06-14 12:05:54 -0400

    card styling and filter zach

--- src/components/Houses.tsx
+++ src/components/Houses.tsx
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
+import { url } from 'inspector';
+import { notStrictEqual } from 'assert';
+
+const cardStyles = {
+    color: '#ffffff',
+    display: 'inline-block',
+    margin: '50px',
+    width: '25%',
+    height: '20%'
+  }
 
 interface House {
     name: string,
     region: string,
     words: string,
     founded: string,
-    currentLord: string,
+    titles: string,
+    seats: string,
+    ancestralWeapons: string
 }
 interface HousesProps{
 }
@@ -26,11 +40,12 @@ class Houses extends Component <HousesProps, HousesState> {
    }
 
      fetchHouses(){
-        fetch("https://www.anapioficeandfire.com/api/houses/?page=1&pageSize=50")
+        fetch(`https://www.anapioficeandfire.com/api/houses?page=1&pageSize=50`)
         .then((results) => results.json())
         .then((results) => this.setState({
             houses: results
         }))
+
     }
     componentDidMount() {
         setTimeout(() => {
@@ -40,20 +55,33 @@ class Houses extends Component <HousesProps, HousesState> {
     render() {
         return(
             <div>
-                <GridList cellHeight={180}>
-                    <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-                    <h1>Houses</h1>
-                    </GridListTile>
-                        {this.state.houses.map((house, index) => (
-                        <GridListTile key={house.name} className="tile">
-                        <h3>{house.name}</h3>
-                        <p>Reigns from: {house.region}</p>
-                        <p>{house.words}</p>
-
-                        </GridListTile> 
-                        ))}
-                </GridList>
+                <h1 style={{color: '#ffffff'}}>Houses</h1>
+                {this.state.houses.map((house, index) => {
+                if (house.words && house.titles[0]) {
+                  return (
+              <Card style={cardStyles} className="card">
+                <CardContent >
+                <Typography variant="h5" component="h2">
+                    {house.name}
+                  </Typography>
+                  <Typography color="textSecondary" gutterBottom style={{color: '#ffffff'}}>
+                  {house.words}
+                  </Typography>
+                  <Typography  color="textSecondary">
+                    {}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {`Reigns from: ${house.region}`}
+                    <br/>
+                  {house.titles[0]}
+                  <br />
+                  </Typography>
+                </CardContent>
+              </Card>
+            )
+          }})}
             </div>
+            
         )
     }
 }
