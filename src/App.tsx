3ea939bd10e28f281ba9572ff03d96c7b0dfd17a
corynnem <corynnem@users.noreply.github.com>
2020-06-14 11:24:35 -0400

    router problems

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,33 @@ import React from 'react';
 import Characters from './components/Characters'
 import Houses from "./components/Houses"
 import Books from './components/Books'
+import SimpleMenu from './home/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Houses />
-      <Characters/>
-      <Books />
+      <Router>
+        <SimpleMenu/>
+        <Switch>
+           <Route exact path = '/'>
+            <Books/>
+          </Route>
+          <Route exact path = '/Houses'/>
+            <Houses />
+          <Route/>
+          <Route exact path = '/Characters'>
+            <Characters/>
+          </Route>
+          </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/home/Navbar.tsx
+++ src/home/Navbar.tsx
@@ -1,1 +1,41 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Link } from 'react-router-dom'
+import Books from '../components/Books';
+import Characters from '../components/Characters';
+import Houses from '../components/Houses';
+
+ const SimpleMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}><Link to = '/'>Books</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link to = '/Characters'>Characters</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link to = '/Houses'>Houses</Link></MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default SimpleMenu;
