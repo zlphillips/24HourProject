bc8769e343004db5f1dc30b432e3ae1b8799c47d
corynnem <corynnem@users.noreply.github.com>
2020-06-15 13:50:41 -0400

    pre-deploy

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,6 @@ import {
   Route,
 } from "react-router-dom";
 import './App.css';
-import Books from './components/Books'
-import Houses from './components/Houses'
-import Characters from './components/Characters'
 
 function App() {
   return (
